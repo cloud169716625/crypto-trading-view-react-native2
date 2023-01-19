a8d6bb6ce2fef245cc20b7d1abbb90fee2dc89a9
suns <suns.dev1213@gmail.com>
2023-01-19 17:03:53 +0300

    completed trade page

--- src/screens/Trades/components/OrderBook/index.js
+++ src/screens/Trades/components/OrderBook/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 // api
 import { getOrderBookAPI } from '../../../../api/trade';
-
 // style
 import styles from './styles';
 
@@ -18,19 +17,31 @@ function OrderBook() {
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.sellContainer}>
+			<View style={styles.itemContainer}>
+				<Text style={styles.amountText}>Price</Text>
+				<Text style={styles.amountText}>Total</Text>
+			</View>
+			<View style={[styles.itemContainer, styles.mb12]}>
+				<Text style={styles.amountText}>{`(USDT)`}</Text>
+				<Text style={styles.amountText}>{`(BTC)`}</Text>
+			</View>
+			<View style={[styles.sellContainer, styles.mb12]}>
 				{orderbookList?.asks?.map((item, key) => (
 					<View style={styles.itemContainer} key={key}>
 						<Text style={styles.sellPrice}>{Number(item[0]).toFixed(2)}</Text>
-						<Text style={styles.amountText}>{item[1]}</Text>
+						<Text style={styles.amountText}>{Number(item[1]).toFixed(3)}</Text>
 					</View>
 				))}
+			</View>
+			<View style={styles.mb12}>
+				<Text style={[styles.buyPrice, {fontSize: 16}]}>16741.6</Text>
+				<Text style={styles.amountText}>16,734.9 USD</Text>
 			</View>
 			<View style={styles.buyContainer}>
 				{orderbookList?.bids?.map((item, key) => (
 					<View style={styles.itemContainer} key={key}>
 						<Text style={styles.buyPrice}>{Number(item[0]).toFixed(2)}</Text>
-						<Text style={styles.amountText}>{item[1]}</Text>
+						<Text style={styles.amountText}>{Number(item[1]).toFixed(3)}</Text>
 					</View>
 				))}
 			</View>
